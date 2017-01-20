e072caf388009d1cd8e50e64b84d486f27fad114
nannany <ymym1990ymym@gmail.com>
2017-01-20 10:04:59 +0900

    Add files via upload
    こみっと

---
+++ src/main/java/expressions/Clause.java
@@ -0,0 +1,13 @@
+package expressions;
+
+public class Clause {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
